77f5a534e822957df6f7312392789c3839929674
юля <@WIN-TDMI2IAM32L>
2022-09-13 16:44:18 +0300

    Добавьте файлы проекта.

---
+++ Matpica/Matrix.cs
@@ -0,0 +1,86 @@
+﻿using Matpica;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matpica
+{
+    internal class Matrix
+    {
+        private int a;
+        private int b;
+        private int[,] matrica;
+        private static Random rand = new Random();
+
+        public Matrix(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            this.matrica = new int[a, b];
+            for (int a1 = 0; a1 < a; a1++)
+            {
+                for (int b1 = 0; b1 < b; b1++)
+                {
+                    this.matrica[a1, b1] = rand.Next(12); 
+                }
+            }
+        }
+       
+        public void Sum(Matrix matrix)
+        {
+           for (int a1 = 0; a1 < this.a; a1++)
+           {
+              for (int b1 = 0; b1 < this.b; b1++)
+              {
+                  this.matrica[a1, b1] = this.matrica[a1, b1] + matrix.matrica[a1, b1];
+              }
+           }
+        }
+      
+        public void PrintMatrix()
+        {
+            for (int a1 = 0; a1 < this.a; a1++)
+            {
+                for (int b1 = 0; b1 < this.b; b1++)
+                {
+                    Console.Write(this.matrica[a1, b1] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+      
+        public void MultiplyOnNumber(int num)
+        {
+            for (int a1 = 0; a1 < this.a; a1++)
+            {
+                for (int b1 = 0; b1 < this.b; b1++)
+                {
+                    this.matrica[a1, b1] = this.matrica[a1, b1] * num;
+                }
+            }
+        }
+        
+        public void MultiplyOnMatrix(Matrix matrix)
+        {
+          int[,] result = new int[this.a, matrix.b];
+          for (int a1 = 0; a1 < this.a; a1++)
+          {
+              for (int b1 = 0; b1 < matrix.b; b1++)
+              {
+                   result[a1, b1] = 0;
+                   for (int c1 = 0; c1 < matrix.a; c1++)
+                   {
+                       result[a1, b1] = result[a1, b1] + this.matrica[a1, c1] * matrix.matrica[c1, b1];
+                   }
+              }
+          }
+          this.matrica = result;
+          this.a = result.GetLength(0);
+          this.b = result.GetLength(1);
+        }
+    }
+}
---
+++ Matpica/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matpica
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Matrix matrica1 = new Matrix(3, 3);
+            Matrix matrica2 = new Matrix(3, 3);
+            Matrix matrica3 = new Matrix(3, 3);
+            Matrix matrica4 = new Matrix(3, 3);
+            Console.WriteLine("\nпервая матрица:\n");
+            matrica1.PrintMatrix();
+            Console.WriteLine("вторая матрица:\n");
+            matrica2.PrintMatrix();
+            Console.WriteLine("сумма матриц 1 и 2:\n");
+            matrica1.Sum(matrica2);
+            matrica1.PrintMatrix();
+            Console.WriteLine("результат умножения сложенных матриц на 2:\n");
+            matrica1.MultiplyOnNumber(2);
+            matrica1.PrintMatrix();
+            Console.WriteLine("третья матрица:\n");
+            matrica3.PrintMatrix();
+            Console.WriteLine("четвёртая матрица:\n");
+            matrica4.PrintMatrix();
+            Console.WriteLine("результат перемножения матриц 3 и 4:\n");
+            matrica3.MultiplyOnMatrix(matrica4);
+            matrica3.PrintMatrix();
+            Console.ReadKey();
+        }
+    }
+}
